11bf7e8cdcfca7ea7dc419e81524e4a9c69fcfc4
himanshu vashistha <113257378+coderstar1234@users.noreply.github.com>
2023-02-19 09:09:21 +0530

    Add files via upload

---
+++ code1.js
@@ -0,0 +1,3 @@
+//You can click on close to close ad, and click on anywhere in the window to bring back ad
+//The ads informations are real, you can click on install to download the app on playstore
+// upvote (+1) if you like it
